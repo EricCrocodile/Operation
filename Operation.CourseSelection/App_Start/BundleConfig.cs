3bbcacb1302ece257b6196220690ce0452249476
Eric <eric_chang@yueyin.com.tw>
2021-11-18 16:13:11 +0800

    Update  Bootstrap to ver 4.6.0  JQuery to ver 3.51 Modify  Views/Shared/_Layout.cshtml  Views/Home/Index.cshtml

--- Operation.CourseSelection/App_Start/BundleConfig.cs
+++ Operation.CourseSelection/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace Operation.CourseSelection
 		public static void RegisterBundles(BundleCollection bundles)
 		{
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-						"~/Scripts/jquery-{version}.js"));
+						"~/Scripts/jquery-{version}.js",						
+						"~/Scripts/bootstrap.bundle.js"));
 
 			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
 						"~/Scripts/jquery.validate*"));
@@ -23,8 +24,7 @@ namespace Operation.CourseSelection
 					  "~/Scripts/bootstrap.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
-					  "~/Content/bootstrap.css",
-					  "~/Content/site.css"));
+					  "~/Content/bootstrap.css"));
 		}
 	}
 }
