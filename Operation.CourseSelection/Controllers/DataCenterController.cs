a0829a94c6d3403aa6f32f6bdcd6903d054fb3c9
Eric <eric_chang@yueyin.com.tw>
2021-11-18 16:56:17 +0800

    ADD  Controllers\DataCenterController Modify  Views\Home\Index.cshtml

---
+++ Operation.CourseSelection/Controllers/DataCenterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Operation.CourseSelection.Controllers
+{
+    public class DataCenterController : Controller
+    {
+        // GET: DataCenter
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
